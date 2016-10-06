f1de4d08e443f5157b1fc6f6203f08a3033d34e7
Hanbyul Jo <hanbyul@nyu.edu>
2016-10-06 18:00:29 -0400

    put example of using Tangram with Leaflet without mapzenjs

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ var React = require('react');
 var Component = React.Component;
 var ReactDOM = require('react-dom');
 
-var mapzenMap = require('mapzen.js')
+var L = require('leaflet');
 
 class Map extends Component {
   componentDidMount() {
     // creates a map with basic buble wrap style
     var lon = -74.009;
     var lat = 40.70531;
-    var map = L.Mapzen.map('map');
+    var map = L.map('map');
+    var layer = Tangram.leafletLayer({
+      scene: 'https://mapzen.com/carto/refill-style/refill-style.yaml',
+      attribution: '<a href="https://mapzen.com/tangram">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/">Mapzen</a>'
+    });
+    layer.addTo(map);
     map.setView([lat, lon], 13);
   }
   render() {
