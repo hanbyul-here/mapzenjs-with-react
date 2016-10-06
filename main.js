bd635b05708be09da5d123ccff1be0714c87e169
Hanbyul Jo <hanbyul@nyu.edu>
2016-10-06 17:07:26 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// main.js
+var React = require('react');
+var Component = React.Component;
+var ReactDOM = require('react-dom');
+
+var mapzenMap = require('mapzen.js')
+
+class Map extends Component {
+  componentDidMount() {
+    // creates a map with the refill basemap
+    var lon = -74.009;
+    var lat = 40.70531;
+    var map = L.Mapzen.map('map');
+    map.setView([lat, lon], 13);
+  }
+  render() {
+    return (
+      <div id = "map" />
+    );
+  }
+}
+
+
+ReactDOM.render(<Map />, document.getElementById('map-wrapper'));
